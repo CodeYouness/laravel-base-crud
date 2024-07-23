13c57bb4daa461d4ea77d49a60db76949bf627cf
CodeYouness <ylijassi@gmail.com>
2024-07-23 20:30:00 +0200

    added card with img of animals

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -9,10 +9,20 @@
     <div class="container">
         <h1 class="text-center">Animal index</h1>
         <div class="row">
+            @foreach ($animals as $animal)
             <div class="col-3">
-
+                <div class="card">
+                    <img src="{{$animal->image}}" class="card-img-top" alt="{{$animal->common_name}}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$animal->scientific_name}}</h5>
+                        <p class="card-text">Also known as: {{$animal->common_name}}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
     </div>
 </main>
 @endsection
+
