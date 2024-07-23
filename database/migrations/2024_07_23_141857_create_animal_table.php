afb9094891717c4242343e831e723dda2e208d03
CodeYouness <ylijassi@gmail.com>
2024-07-23 16:32:03 +0200

    added migration

---
+++ database/migrations/2024_07_23_141857_create_animal_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animal', function (Blueprint $table) {
+            $table->id();
+            $table->string('common_name', 100);
+            $table->string('scientific_name', 100);
+            $table->string('habitat', 100);
+            $table->string('class', 100);
+            $table->string('family', 100);
+            $table->tinyInteger('average_life')->unsigned();
+            $table->tinyInteger('average_weight')->unsigned();
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animal');
+    }
+};
